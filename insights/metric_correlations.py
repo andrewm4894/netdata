de64087400ec1a29584e8d71fe837c6c3fcff07e
Andrew Maguire <andrewm4894@gmail.com>
2020-06-17 11:15:36 +0100

    dev

--- insights/metric_correlations.py
+++ insights/metric_correlations.py
@@ -1,7 +1,37 @@
 #!/usr/bin/python3
+import argparse
+import logging
 
-import sys
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
-for line in sys.stdin:
-    print(line)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--host', type=str, nargs='?', help='host', default='127.0.0.1'
+)
+parser.add_argument(
+    '--port', type=str, nargs='?', help='port', default='19999'
+)
+parser.add_argument(
+    '--baseline_after', type=str, nargs='?', help='baseline_after', default='-120'
+)
+parser.add_argument(
+    '--baseline_before', type=str, nargs='?', help='baseline_before', default='-60'
+)
+parser.add_argument(
+    '--highlight_after', type=str, nargs='?', help='highlight_after', default='-60'
+)
+parser.add_argument(
+    '--highlight_before', type=str, nargs='?', help='highlight_before', default='0'
+)
+args = parser.parse_args()
 
+# parse args
+host = args.host
+port = args.port
+baseline_after = args.baseline_after
+baseline_before = args.baseline_before
+highlight_after = args.highlight_after
+highlight_before = args.highlight_before
+
+log.info(f"args={args}")
