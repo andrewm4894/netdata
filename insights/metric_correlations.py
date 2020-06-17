94a578245253fa4023ba056e75d1427705820dde
Andrew Maguire <andrewm4894@gmail.com>
2020-06-17 16:04:46 +0100

    dev

--- insights/metric_correlations.py
+++ insights/metric_correlations.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import logging
 import time
 
@@ -6,100 +7,99 @@ from netdata_pandas.data import get_data, get_chart_list
 from insights_modules.model import run_model
 
 
-time_start = time.time()
+def main():
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--host', type=str, nargs='?', help='host', default='127.0.0.1:19999'
-)
-parser.add_argument(
-    '--baseline_after', type=str, nargs='?', help='baseline_after', default='-120'
-)
-parser.add_argument(
-    '--baseline_before', type=str, nargs='?', help='baseline_before', default='-60'
-)
-parser.add_argument(
-    '--highlight_after', type=str, nargs='?', help='highlight_after', default='-60'
-)
-parser.add_argument(
-    '--highlight_before', type=str, nargs='?', help='highlight_before', default='0'
-)
-parser.add_argument(
-    '--model', type=str, nargs='?', help='model', default='ks'
-)
-parser.add_argument(
-    '--n_lags', type=str, nargs='?', help='n_lags', default='2'
-)
-parser.add_argument(
-    '--log_level', type=str, nargs='?', help='log_level', default='info'
-)
-args = parser.parse_args()
+    time_start = time.time()
 
-# parse args
-host = args.host
-baseline_after = int(args.baseline_after)
-baseline_before = int(args.baseline_before)
-highlight_after = int(args.highlight_after)
-highlight_before = int(args.highlight_before)
-model = args.model
-n_lags = int(args.n_lags)
-log_level = args.log_level
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, nargs='?', help='host', default='127.0.0.1:19999')
+    parser.add_argument('--baseline_after', type=str, nargs='?', help='baseline_after', default='-240')
+    parser.add_argument('--baseline_before', type=str, nargs='?', help='baseline_before', default='-120')
+    parser.add_argument('--highlight_after', type=str, nargs='?', help='highlight_after', default='-120')
+    parser.add_argument('--highlight_before', type=str, nargs='?', help='highlight_before', default='0')
+    parser.add_argument('--model', type=str, nargs='?', help='model', default='ks')
+    parser.add_argument('--n_lags', type=str, nargs='?', help='n_lags', default='2')
+    parser.add_argument('--log_level', type=str, nargs='?', help='log_level', default='info')
+    parser.add_argument('--results_file', type=str, nargs='?', help='results_file', default='results.json')
+    args = parser.parse_args()
+    host = args.host
+    baseline_after = int(args.baseline_after)
+    baseline_before = int(args.baseline_before)
+    highlight_after = int(args.highlight_after)
+    highlight_before = int(args.highlight_before)
+    model = args.model
+    n_lags = int(args.n_lags)
+    log_level = args.log_level
+    results_file = args.results_file
 
-# handle 'after' and 'before' values if passed in as relative
-now = time.time()
-if baseline_after <= 0:
-    baseline_after = int(now + baseline_after)
-if baseline_before <= 0:
-    baseline_before = int(now + baseline_before)
-if highlight_after <= 0:
-    highlight_after = int(now + highlight_after)
-if highlight_before <= 0:
-    highlight_before = int(now + highlight_before)
+    # set up logging
+    if log_level == 'info':
+        logging.basicConfig(level=logging.INFO)
+    elif log_level == 'debug':
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.WARN)
+    log = logging.getLogger(__name__)
 
-# set up logging
-if log_level == 'info':
-    logging.basicConfig(level=logging.INFO)
-elif log_level == 'debug':
-    logging.basicConfig(level=logging.DEBUG)
-else:
-    logging.basicConfig(level=logging.WARN)
+    # handle 'after' and 'before' values if passed in as relative integers
+    if baseline_after <= 0:
+        baseline_after = int(time_start + baseline_after)
+    if baseline_before <= 0:
+        baseline_before = int(time_start + baseline_before)
+    if highlight_after <= 0:
+        highlight_after = int(time_start + highlight_after)
+    if highlight_before <= 0:
+        highlight_before = int(time_start + highlight_before)
 
-log = logging.getLogger(__name__)
+    log.info(f"... args={args}")
 
-log.info(f"... args={args}")
+    log.debug(f"... baseline_after={baseline_after}")
+    log.debug(f"... baseline_before={baseline_before}")
+    log.debug(f"... highlight_after={highlight_after}")
+    log.debug(f"... highlight_before={highlight_before}")
 
-log.debug(f"... baseline_after={baseline_after}")
-log.debug(f"... baseline_before={baseline_before}")
-log.debug(f"... highlight_after={highlight_after}")
-log.debug(f"... highlight_before={highlight_before}")
+    # get charts
+    charts = get_chart_list(host)
 
-# get charts
-charts = get_chart_list(host)
+    # get data
+    df = get_data(host, charts, after=baseline_after, before=highlight_before, diff=True,
+                  ffill=True, numeric_only=True, nunique_thold=0.05, col_sep='|')
 
-# get data
-df = get_data(host, charts, after=baseline_after, before=highlight_before, diff=True,
-              ffill=True, numeric_only=True, nunique_thold=0.05, col_sep='|')
+    log.info(f"... df.shape={df.shape}")
+    log.debug(f"... df.head()={df.head()}")
 
-log.info(f"... df.shape={df.shape}")
+    # get numpy arrays
+    colnames = list(df.columns)
+    arr_baseline = df.query(f'{baseline_after} <= time_idx <= {baseline_before}').values
+    arr_highlight = df.query(f'{highlight_after} <= time_idx <= {highlight_before}').values
+    charts = list(set([col.split('|')[0] for col in colnames]))
 
-# get numpy arrays
-colnames = list(df.columns)
-arr_baseline = df.query(f'{baseline_after} <= time_idx <= {baseline_before}').values
-arr_highlight = df.query(f'{highlight_after} <= time_idx <= {highlight_before}').values
-charts = list(set([col.split('|')[0] for col in colnames]))
+    log.debug(f'... charts = {charts}')
+    log.debug(f'... colnames = {colnames}')
+    log.debug(f'... arr_baseline.shape = {arr_baseline.shape}')
+    log.debug(f'... arr_highlight.shape = {arr_highlight.shape}')
 
-log.debug(f'... arr_baseline.shape = {arr_baseline.shape}')
-log.debug(f'... arr_highlight.shape = {arr_highlight.shape}')
+    time_got_data = time.time()
+    log.info(f'... {round(time_got_data - time_start,2)} seconds to get data.')
 
-time_got_data = time.time()
-log.info(f'... {round(time_got_data - time_start,2)} seconds to get data.')
+    # get scores
+    results = run_model(model, colnames, arr_baseline, arr_highlight, n_lags)
 
-# get scores
-results = run_model(model, colnames, arr_baseline, arr_highlight, n_lags)
+    time_got_scores = time.time()
+    log.info(f'... {round(time_got_scores - time_got_data,2)} seconds to get scores.')
 
-time_got_scores = time.time()
-log.info(f'... {round(time_got_scores - time_got_data,2)} seconds to get scores.')
+    if results_file:
+        with open(results_file, 'w', encoding='utf-8') as f:
+            json.dump(results, f, ensure_ascii=False, indent=4)
 
-print(results)
+    print(results)
+
+    time_done = time.time()
+    log.info(f'... {round(time_done - time_start, 2)} seconds in total.')
+
+
+if __name__ == '__main__':
+    main()
 
 
