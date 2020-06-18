1c592c91ed33d6da7f3f94d5057e69bd9bbf7c4c
Andrew Maguire <andrewm4894@gmail.com>
2020-06-18 15:05:31 +0100

    adding benchmark script

--- insights/run_benchmarks.py
+++ insights/run_benchmarks.py
@@ -8,17 +8,19 @@ import pandas as pd
 from metric_correlations import run_metric_correlations
 
 
-def run_benchmarks(host=None, model_list=None, n_list=None):
+def run_benchmarks(host=None, model_list=None, n_list=None, sleep_secs=None):
 
     # parse args, arg may come in via command line or via a function call.
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', type=str, nargs='?', help='host', default='127.0.0.1:19999')
     parser.add_argument('--model_list', type=str, nargs='?', help='model_list', default='ks,hbos')
     parser.add_argument('--n_list', type=str, nargs='?', help='n_list', default='100,1000,5000,10000')
+    parser.add_argument('--sleep_secs', type=str, nargs='?', help='sleep_secs', default='2')
     args = parser.parse_args()
     host = args.host if not host else host
     model_list = args.model_list if not model_list else model_list
     n_list = args.n_list if not n_list else n_list
+    sleep_secs = float(args.sleep_secs) if not sleep_secs else float(sleep_secs)
 
     model_list = model_list.split(',')
     n_list = [int(n) for n in n_list.split(',')]
@@ -43,6 +45,9 @@ def run_benchmarks(host=None, model_list=None, n_list=None):
             time_scores = float(re.search(" (.*) seconds to get scores", results).group(1))
             time_total = float(re.search(" (.*) seconds in total", results).group(1))
             results_all.append([model, n, time_data, time_scores, time_total])
+            # sleep for a while so you can clearly see profile of each model in your netdata dashboard :)
+            if sleep_secs > 0.0:
+                time.sleep(sleep_secs)
 
     df_results = pd.DataFrame(results_all, columns=['model', 'n', 'time_data', 'time_scores', 'time_total'])
     print('---results---')
