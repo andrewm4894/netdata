54206010ec712c18890121fc6652f2e027dceb33
Andrew Maguire <andrewm4894@gmail.com>
2020-06-23 15:24:03 +0100

    add some testing

--- insights/test_metric_correlations.py
+++ insights/test_metric_correlations.py
@@ -10,17 +10,17 @@ test_host = 'london.my-netdata.io'
 test_host_charts_available = set(get_chart_list(host=test_host))
 
 
-def do_test(model):
+def do_test(host, model):
     f = io.StringIO()
     with redirect_stdout(f):
-        run_metric_correlations(model=model)
+        run_metric_correlations(host=host, model=model)
     results = f.getvalue()
     results = json.loads(results)
     return results
 
 
 def test_ks():
-    results = do_test('ks')
+    results = do_test(host=test_host, model='ks')
     charts_scored = set(results.keys())
     print(charts_scored)
     print('----------')
