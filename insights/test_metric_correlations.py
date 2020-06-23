d6070443d69a48858b6f1b2b25f768ce7d8cdb40
Andrew Maguire <andrewm4894@gmail.com>
2020-06-23 15:13:06 +0100

    add some testing

--- insights/test_metric_correlations.py
+++ insights/test_metric_correlations.py
@@ -5,13 +5,18 @@ from contextlib import redirect_stdout
 from metric_correlations import run_metric_correlations
 
 
-def test_ks():
+def do_test(model):
     f = io.StringIO()
     with redirect_stdout(f):
-        run_metric_correlations()
+        run_metric_correlations(model=model)
     results = f.getvalue()
-    results_json = json.loads(results)
-    print(results_json.keys())
+    results = json.loads(results)
+    return results
+
+
+def test_ks():
+    results = do_test('ks')
+    print(results.keys())
     assert 1 == 1
     assert 1 == 2
 
