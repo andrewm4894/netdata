9b88f9737d7907f19a5e6a70ead825801a58e237
Andrew Maguire <andrewm4894@gmail.com>
2020-06-23 15:16:57 +0100

    add some testing

--- insights/test_metric_correlations.py
+++ insights/test_metric_correlations.py
@@ -3,6 +3,11 @@ import json
 from contextlib import redirect_stdout
 
 from metric_correlations import run_metric_correlations
+from netdata_pandas.data import get_chart_list
+
+# define some test inputs
+test_host = 'london.my-netdata.io'
+test_host_charts_available = get_chart_list(host=test_host)
 
 
 def do_test(model):
@@ -16,7 +21,8 @@ def do_test(model):
 
 def test_ks():
     results = do_test('ks')
-    print(results.keys())
+    print(set(results.keys()))
+    print(set(test_host_charts_available))
     assert 1 == 1
     assert 1 == 2
 
