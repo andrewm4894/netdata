27c2785b573f60e98b32f9793ef7047db2af36a5
Andrew Maguire <andrewm4894@gmail.com>
2020-06-23 15:32:35 +0100

    add some testing

--- insights/test_metric_correlations.py
+++ insights/test_metric_correlations.py
@@ -10,23 +10,27 @@ test_host = 'london.my-netdata.io'
 test_host_charts_available = set(get_chart_list(host=test_host))
 min_result_len = 10
 
-tests = {
-    {"host": test_host, "model": "ks"},
-    {"host": test_host, "model": "hbos"},
-}
 
-
-def test_metric_correlations(host, model):
+def do_test(host, model):
     f = io.StringIO()
     with redirect_stdout(f):
         run_metric_correlations(host=host, model=model)
     results = f.getvalue()
     results = json.loads(results)
+    return results
+
+
+def validate_results(results):
     charts_scored = set(results.keys())
     assert len(results) >= min_result_len
     assert charts_scored.issubset(test_host_charts_available)
 
-# run tests
-for test in tests:
-    test_metric_correlations(host=test['host'], model=test['model'])
+
+def test_ks_default():
+    results = do_test(host=test_host, model='ks')
+    validate_results(results)
+
+
+
+
 
