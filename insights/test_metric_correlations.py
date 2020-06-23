59fca33c4fe2eae1fefb7f752c7e267ca62d757c
Andrew Maguire <andrewm4894@gmail.com>
2020-06-23 15:44:01 +0100

    add some testing

--- insights/test_metric_correlations.py
+++ insights/test_metric_correlations.py
@@ -42,7 +42,7 @@ def validate_results(results, model_level):
 def test_hbos_chart(model_level='chart'):
     results = do_test(host=test_host, model='hbos', model_level=model_level)
     validate_results(results, model_level)
-    print([results[chart][0] for chart in results])
+    print([results[chart]['*'] for chart in results])
     assert 1 == 1
 
 
