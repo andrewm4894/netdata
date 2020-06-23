6b19122680209326d139e544ff79c451c27f0121
Andrew Maguire <andrewm4894@gmail.com>
2020-06-23 13:53:50 +0100

    adding adtk lib and refactoring a bit

--- insights/insights_modules/model.py
+++ insights/insights_modules/model.py
@@ -7,7 +7,7 @@ from insights_modules.model_mp import do_mp, mp_models_supported
 from insights_modules.model_pyod import do_pyod, pyod_models_supported
 
 # filter some future warnings from sklearn and numba that come via pyod
-warnings.simplefilter(action='ignore', category=FutureWarning)
+#warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.filterwarnings(action='ignore', message='Insufficiently recent colorama version found. Numba requires colorama >= 0.3.9')
 
 log = logging.getLogger(__name__)
