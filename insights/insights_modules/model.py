612e971233d9edce98ad67483b7356baa0de73a6
Andrew Maguire <andrewm4894@gmail.com>
2020-06-25 12:45:58 +0100

    adding validate_inputs function

--- insights/insights_modules/model.py
+++ insights/insights_modules/model.py
@@ -11,7 +11,7 @@ from insights_modules.model_pyod import do_pyod, pyod_models_supported
 
 log = logging.getLogger(__name__)
 
-chart_level_models = pyod_models_supported + adtk_models_supported
+models_chart_level = pyod_models_supported + adtk_models_chart_level
 models_supported = ['ks'] + mp_models_supported + pyod_models_supported + adtk_models_supported
 
 
@@ -31,7 +31,7 @@ def run_model(model, colnames, arr_baseline, arr_highlight, n_lags=0, model_erro
 
 
 def validate_inputs(model, model_level):
-    if model not in adtk_models_chart_level and model_level == 'chart':
+    if model not in models_chart_level and model_level == 'chart':
         model_level = 'dim'
     return model_level
 
