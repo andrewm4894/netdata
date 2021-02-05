f72166cbe0c380a228132f30167f9783dd3c4cc3
Andrew Maguire <andrewm4894@gmail.com>
2021-02-05 14:53:24 +0000

    dev

--- collectors/python.d.plugin/anomalies/anomalies.chart.py
+++ collectors/python.d.plugin/anomalies/anomalies.chart.py
@@ -350,6 +350,8 @@ class Service(SimpleService):
             self.validate_charts('probability', data_probability, divisor=100)
             self.validate_charts('anomaly', data_anomaly)
 
+        self.info(data)
+
         self.data_latest = data
 
         return data
