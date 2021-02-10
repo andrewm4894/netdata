a7367ac4ef9a6929bfcef1e66e983c0489857030
Andrew Maguire <andrewm4894@gmail.com>
2021-02-10 14:24:55 +0000

    dev

--- collectors/python.d.plugin/anomalies/anomalies.chart.py
+++ collectors/python.d.plugin/anomalies/anomalies.chart.py
@@ -366,16 +366,16 @@ class Service(SimpleService):
                 )
                 data_probability[model_display_name + '_prob'] = np.nan_to_num(self.models[model].predict_proba(X_model)[-1][1]) * 10000
                 data_anomaly[model_display_name + '_anomaly'] = self.models[model].predict(X_model)[-1]
-            except Exception as e:
-                self.info('self.df_allmetrics:')
-                self.info(self.df_allmetrics[self.df_allmetrics.columns[self.df_allmetrics.columns.str.startswith(f'{model}|')]])
-                self.info(e)
+            except Exception as _:
+                #self.info('self.df_allmetrics:')
+                #self.info(self.df_allmetrics[self.df_allmetrics.columns[self.df_allmetrics.columns.str.startswith(f'{model}|')]])
+                #self.info(e)
                 if model_display_name + '_prob' in self.data_latest:
-                    self.info(f'prediction failed for {model} at run_counter {self.runs_counter}, using last prediction instead.')
+                    #self.info(f'prediction failed for {model} at run_counter {self.runs_counter}, using last prediction instead.')
                     data_probability[model_display_name + '_prob'] = self.data_latest[model_display_name + '_prob']
                     data_anomaly[model_display_name + '_anomaly'] = self.data_latest[model_display_name + '_anomaly']
                 else:
-                    self.info(f'prediction failed for {model} at run_counter {self.runs_counter}, skipping as no previous prediction.')
+                    #self.info(f'prediction failed for {model} at run_counter {self.runs_counter}, skipping as no previous prediction.')
                     continue
 
         return data_probability, data_anomaly
@@ -417,7 +417,7 @@ class Service(SimpleService):
 
         self.save_data_latest(data, data_probability, data_anomaly)
 
-        self.info(f'len(data)={len(data)}')
+        #self.info(f'len(data)={len(data)}')
         #self.info(f'data')
 
         return data
