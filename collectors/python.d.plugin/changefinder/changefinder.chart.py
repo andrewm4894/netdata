fbacdc869407e92bca8674fe005b265c161884a2
Andrew Maguire <andrewm4894@gmail.com>
2021-02-18 14:54:58 +0000

    dev

--- collectors/python.d.plugin/changefinder/changefinder.chart.py
+++ collectors/python.d.plugin/changefinder/changefinder.chart.py
@@ -33,7 +33,8 @@ def charts_template():
 
 DEFAULT_PROTOCOL = 'http'
 DEFAULT_HOST = '127.0.0.1:19999'
-DEFAULT_CHARTS_REGEX = 'system.*'
+DEFAULT_CHARTS_REGEX = 'system.cpu'
+DEFAULT_MODE = 'per_dim'
 
 
 class Service(UrlService):
@@ -43,8 +44,10 @@ class Service(UrlService):
         self.host = self.configuration.get('host', DEFAULT_HOST)
         self.protocol = self.configuration.get('protocol', DEFAULT_PROTOCOL)
         self.charts_regex = re.compile(self.configuration.get('charts_regex', DEFAULT_CHARTS_REGEX))
-        self.models = {}
         self.url = '{}://{}/api/v1/allmetrics?format=json'.format(self.protocol, self.host)
+        self.models = {}
+        self.mode = self.configuration.get('mode', DEFAULT_MODE)
+        
 
     def _get_data(self):
         raw_data = self._get_raw_data()
@@ -55,15 +58,34 @@ class Service(UrlService):
         charts = list(filter(self.charts_regex.match, raw_data.keys()))
         data = {}
         for chart in charts:
-            x = [raw_data[chart]['dimensions'][x]['value'] for x in raw_data[chart]['dimensions']]
-            x = [x for x in x if x is not None]
-            x = sum(x) / len(x)
 
-            if chart not in self.models:
-                self.models[chart] = changefinder.ChangeFinder()
+            if self.mode == 'per_chart':
 
-            score, _ = self.models[chart].update(x)
-            data[chart] = score * 100
+                x = [raw_data[chart]['dimensions'][x]['value'] for x in raw_data[chart]['dimensions']]
+                x = [x for x in x if x is not None]
+                x = sum(x) / len(x)
+
+                if chart not in self.models:
+                    self.models[chart] = changefinder.ChangeFinder()
+
+                score, _ = self.models[chart].update(x)
+                data[chart] = score * 100
+
+            else:
+
+                for dim in raw_data[chart]['dimensions']:
+
+                    model = '{}|{}'.format(chart,dim)
+
+                    if model not in self.models:
+                        self.models[model] = changefinder.ChangeFinder()
+
+                        x = raw_data[chart]['dimensions'][dim]['value']
+
+                        if x is not None:
+
+                            score, _ = self.models[model].update(x)
+                            data[model] = score * 100
         
         self.update_chart('changefinder', data)
 
